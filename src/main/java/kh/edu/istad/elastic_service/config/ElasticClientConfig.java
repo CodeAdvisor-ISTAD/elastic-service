6d83b36d10466cb4a22672fc50ea23738a433da3
MengseuThoeng <mengseu2004@gmail.com>
2025-02-07 20:45:05 +0700

    Fix URL

--- src/main/java/kh/edu/istad/elastic_service/config/ElasticClientConfig.java
+++ src/main/java/kh/edu/istad/elastic_service/config/ElasticClientConfig.java
@@ -1,19 +1,19 @@
-package kh.edu.istad.elastic_service.config;
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
-
-@Configuration
-public class ElasticClientConfig extends ElasticsearchConfiguration {
-
-
-    @Override
-    public ClientConfiguration clientConfiguration() {
-        return ClientConfiguration.builder()
-                .connectedTo("http://202.178.125.77:1200")
-                .build();
-    }
-}
+//package kh.edu.istad.elastic_service.config;
+//
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.elasticsearch.client.ClientConfiguration;
+//import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+//
+//@Configuration
+//public class ElasticClientConfig extends ElasticsearchConfiguration {
+//
+//
+//    @Override
+//    public ClientConfiguration clientConfiguration() {
+//        return ClientConfiguration.builder()
+//                .connectedTo("https://elastic.panda.engineer/")
+//                .build();
+//    }
+//}
